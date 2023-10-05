f3e2dd6bd38fd25c2e6e2d12e9663b3db1c0dda6
slma27myloo <147081253+slma27myloo@users.noreply.github.com>
2023-10-05 22:36:47 +0700

    Add files via upload

---
+++ Salma Sesi 2_Pemograman Berbasis Platfom.js
@@ -0,0 +1,13 @@
+var fulltName = "Salma Salsabila Nur Anisa"
+var lastName = "Salma"
+var asalKampus = "Universitas Nusa Putra"
+var asalProdi = "Program Studi Teknik Informatika"
+var usia = 19
+var isMarried = false
+
+console.log(fulltName);
+console.log(lastName);
+console.log(asalKampus);
+console.log(asalProdi);
+console.log ("Usia:",usia);
+console.log("isMarried:", isMarried);
